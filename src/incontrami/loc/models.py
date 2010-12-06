2283e2447d50f5f7d6edae42b93ae4e8a35ad522
Lorenzo Viscanti <lorenzo@mikamai.com>
2010-12-06 20:57:48 +0100

    django app

--- src/incontrami/loc/models.py
+++ src/incontrami/loc/models.py
@@ -5,55 +5,14 @@ from random import choice
 # Create your models here.
 
 
-class Place(models.Model):
-	name = models.CharField(max_length = 255)
-	foursquare_id = models.IntegerField(blank = True, db_index = True, null = True)
-	address = models.CharField(max_length = 255)
-	geolong = models.DecimalField('longitude', max_digits=13, decimal_places=10, blank=True, null=True, db_index = True)
-	geolat = models.DecimalField('latitude', max_digits=13, decimal_places=10, blank=True, null=True, db_index = True)
-	city = models.CharField(max_length = 255, blank = True)
-	foursquare_category = models.CharField(max_length = 255, blank = True, null = True)
-	validated = models.BooleanField(default = False)
-	category = models.SmallIntegerField(null = True, blank = True, default = 0, db_index = True)
-	points = models.SmallIntegerField(default = 0)
-	phone = models.CharField(max_length = 64, null = True, blank  = True)
-	site = models.CharField(max_length = 128, null = True, blank = True)
-	google_place_id = models.CharField(max_length = 128, null = True, blank = True)
-	google_category = models.CharField(max_length = 128, blank = True, null = True)
-	
-	def slug(self):
-		if self.city != None:
-			return slugify(self.city + ' ' +self.name)
-		else:
-			return slugify(self.name)
-			
-class Positions(models.Model):
-	geolong = models.DecimalField('longitude', max_digits=13, decimal_places=10, blank=True, null=True, db_index = True)
-	geolat = models.DecimalField('latitude', max_digits=13, decimal_places=10, blank=True, null=True, db_index = True)
-	date = models.DateTimeField(auto_now_add = True)
-	
 class FacebookUser(models.Model):
 	facebook_id = models.CharField(max_length=100, unique=True)
 	contrib_user = models.OneToOneField(User)
 	contrib_password = models.CharField(max_length=100)
 
 
-class Like(models.Model):
-	place = models.ForeignKey(Place, db_index = True)
-	user = models.ForeignKey(User)
-	date = models.DateTimeField(auto_now_add = True)
-	valid = models.BooleanField(default = True)
-	
 class UserProfile(models.Model):
 	user = models.ForeignKey(User, unique = True, db_index = True)
-
-class Tip(models.Model):
-	place = models.ForeignKey(Place, db_index = True)
-	user = models.ForeignKey(User, null = True, blank = True, db_index = True)
-	source = models.SmallIntegerField(default = 0) #0=internal, 1=Forusquare
-	url = models.CharField(max_length = 255, blank = True, null = True)
-	text = models.TextField()
-	date = models.DateTimeField(auto_now_add = True)
 	
 
 class FacebookSessionError(Exception):   
