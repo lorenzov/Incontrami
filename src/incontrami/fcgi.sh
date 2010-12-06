ea9bfe6e491ddb908a8bc535650e864121f43423
Lorenzo Viscanti <lorenzo@mikamai.com>
2010-12-06 20:42:01 +0100

    django app

--- src/incontrami/fcgi.sh
+++ src/incontrami/fcgi.sh
@@ -1,13 +1,13 @@
 # Replace these three settings.
-PROJDIR="/home/ubuntu/geniusloci/src/geniusloci"
+PROJDIR="/home/ubuntu/incontrami/src/incontrami"
 PIDFILE="$PROJDIR/mysite.pid"
-SOCKET="/home/ubuntu/geniusloci.sock"
-export PYTHONPATH=/home/ubuntu/geniusloci/src/:/var/django/
-export DJANGO_SETTINGS_MODULE=geniusloci.settings
+SOCKET="/home/ubuntu/incontrami.sock"
+export PYTHONPATH=/home/ubuntu/incontrami/src/:/var/django/
+export DJANGO_SETTINGS_MODULE=incontrami.settings
 cd $PROJDIR
 if [ -f $PIDFILE ]; then
     kill `cat -- $PIDFILE`
     rm -f -- $PIDFILE
 fi
 
-exec ./manage.py runfcgi  method=threaded host=127.0.0.1 port=3034 pidfile=$PIDFILE
+exec ./manage.py runfcgi  method=threaded host=127.0.0.1 port=3035 pidfile=$PIDFILE
